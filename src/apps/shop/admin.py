2f458425ecec129d4d2fafd173cc537fba84e0c5
Egor <wzxv_@outlook.com>
2023-09-11 14:22:23 +0300

    allows to upload image for product through admin panel

--- src/apps/shop/admin.py
+++ src/apps/shop/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import Product, DeliveryPoint, Order, Feedback
+from .models import Product, DeliveryPoint, Order, Feedback, ProductImage
+from django.utils.html import escape, format_html
+
+
+class ProductImageInline(admin.TabularInline):  # You can use StackedInline if you prefer a different layout
+    model = ProductImage
+    extra = 1  # Number of empty image forms to display
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'quantity']
+    list_display = ['id', 'name', 'price', 'quantity', 'display_image']
     search_fields = ('name',)
+    inlines = [ProductImageInline]
+
+    def display_image(self, obj):
+        if obj.images.exists():
+            image_url = obj.images.first().url.url  # Assuming you have at least one image per product
+            return format_html('<img src="{}" width="50" height="50" />', escape(image_url))
+        return format_html('<img src="{}" width="50" height="50" />', escape('path_to_placeholder_image.jpg'))
+
+    display_image.short_description = 'Image'
 
 
 @admin.register(DeliveryPoint)
