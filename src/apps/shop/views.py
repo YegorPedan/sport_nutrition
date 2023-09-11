2a39d8ac822ad8358856f70ec787bc577a361a9b
Egor <wzxv_@outlook.com>
2023-09-12 01:25:44 +0300

    Create page for each product

--- src/apps/shop/views.py
+++ src/apps/shop/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView, DeleteView
 
 from .models import Product
 
@@ -9,3 +9,10 @@ class Index(ListView):
     template_name = 'shop/products.html'
     model = Product
     context_object_name = 'products'
+    paginate_by = 3
+
+
+class ShowProduct(DeleteView):
+    model = Product
+    template_name = 'shop/certain_product.html'
+    context_object_name = 'product'
