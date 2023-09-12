051901c5f61041212df92b9882e4036b74978e8b
Egor <wzxv_@outlook.com>
2023-09-12 14:13:54 +0300

    Sorting by price has been created!

--- src/apps/shop/views.py
+++ src/apps/shop/views.py
@@ -11,6 +11,19 @@ class Index(ListView):
     context_object_name = 'products'
     paginate_by = 3
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+
+        return queryset
+
 
 class ShowProduct(DeleteView):
     model = Product
